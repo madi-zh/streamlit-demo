18ee0fccb8b51ed9127d5d2cca5449fd61b146f6
Madi Zhanbyrtayev <madi.zhanbyrtayev@nexxiot.com>
2024-11-29 19:50:34 +0500

    feat(root): test commit to see changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("Hello, Streamlit with Poetry!")
-st.write("This is a sample Streamlit app running with Poetry.")
+st.write("This is a sample Streamlit app running with Poetry. Testing how fast the data can be deployed")
