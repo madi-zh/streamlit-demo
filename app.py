b8230d0695b2587b0a95230ead7f85f999b9b956
Madi Zhanbyrtayev <madi.zhanbyrtayev@nexxiot.com>
2024-11-29 19:51:29 +0500

    refactor(root): isort, black and pytest run

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.title("Hello, Streamlit with Poetry!")
-st.write("This is a sample Streamlit app running with Poetry. Testing how fast the data can be deployed")
+st.write(
+    "This is a sample Streamlit app running with Poetry. Testing how fast the data can be deployed"
+)
